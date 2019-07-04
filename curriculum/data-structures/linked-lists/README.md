c128242fefe77374a1e9cd6f75d8e7a9b1b436b9
John Cokos <highlander.44@gmail.com>
2019-07-04 14:04:27 -0700

    Linked List Demo and Notes Uploaded

---
+++ curriculum/data-structures/linked-lists/README.md
@@ -0,0 +1,3 @@
+# Linked Lists
+
+Please read the [Code Fellows Linked List Notes](https://codefellows.github.io/common_curriculum/data_structures_and_algorithms/Code_401/class-05/resources/singly_linked_list) for a detailed recap.
---
+++ curriculum/data-structures/linked-lists/linked-lists.js
@@ -0,0 +1,39 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    if(!this.head) {
+      this.head = new Node(value);
+      return;
+    }
+
+    let current = this.head;
+    while(current.next) {
+      current = current.next;
+    }
+
+    current.next = new Node(value);
+    this.tail = current.next;
+
+  }
+}
+
+let list = new LinkedList();
+list.append('john');
+list.append('cat');
+list.append('zach');
+list.append('allie');
+
+console.log(list);
